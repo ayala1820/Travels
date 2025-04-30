c734ea90aba25b86ba7afcc954db823d03851723
Ayala <1820ayala@gmail.com>
2025-04-30 15:15:42 +0300

    Add Dto

--- Travels/Travels-Server/Core/Dto/ActivityDto.cs
+++ Travels/Travels-Server/Core/Dto/ActivityDto.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Services
+namespace Core.Dto
 {
-    public interface IService<T>
+    public class ActivityDto
     {
-        
     }
 }
--- Travels/Travels-Server/Core/Repositories/IRepository.cs
+++ Travels/Travels-Server/Core/Repositories/IRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.Repositories
 {
-    internal class IRepository
+    public class IRepository
     {
     }
 }
